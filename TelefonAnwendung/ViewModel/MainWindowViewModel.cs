8520550bbea5e4f1b7903d23f9da244e24de10d1
Menrma <menrathmarkus@gmail.com>
2018-04-11 16:55:51 +0200

    View und ViewModel anlegen und verknüpfen

---
+++ TelefonAnwendung/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelefonAnwendung.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        #region Properties
+
+        #endregion
+
+        #region Dependecy Properties
+        public string TestText { get; set; } = "Das ist ein Test";
+        #endregion
+    }
+}
