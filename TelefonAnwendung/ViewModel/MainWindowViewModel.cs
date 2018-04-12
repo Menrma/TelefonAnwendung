e7c39362f7ea31076710231330f3acc9104efdc3
Menrma <menrathmarkus@gmail.com>
2018-04-12 11:39:37 +0200

    Neue Dependecy Property im ViewModel

--- TelefonAnwendung/ViewModel/MainWindowViewModel.cs
+++ TelefonAnwendung/ViewModel/MainWindowViewModel.cs
@@ -10,16 +10,22 @@ namespace TelefonAnwendung.ViewModel
     public class MainWindowViewModel
     {
         #region Properties
-        private Visibility _callButtonIsVisible = Visibility.Visible;
+        private string _telefonnummerEingabe = String.Empty;
+        private Visibility _callButtonIsVisible = Visibility.Hidden;
         #endregion
 
         #region Dependecy Properties
-        public string TestText { get; set; } = "Das ist ein Test";
+        public string TelefonnummerEingabe
+        {
+            get { return _telefonnummerEingabe; }
+            set { _telefonnummerEingabe = value; }
+        }
         public Visibility CallButtonIsVisible
         {
             get { return _callButtonIsVisible; }
             set { _callButtonIsVisible = value; }
         }
+
         #endregion
     }
 }
