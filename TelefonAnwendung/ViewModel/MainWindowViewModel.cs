72f5ca8791ddee2b6916d086b7e89a98e761b114
Menrma <menrathmarkus@gmail.com>
2018-04-12 10:42:52 +0200

    Benutzeroberfläche um Label und Buttons erweitern

--- TelefonAnwendung/ViewModel/MainWindowViewModel.cs
+++ TelefonAnwendung/ViewModel/MainWindowViewModel.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace TelefonAnwendung.ViewModel
 {
     public class MainWindowViewModel
     {
         #region Properties
-
+        private Visibility _callButtonIsVisible = Visibility.Visible;
         #endregion
 
         #region Dependecy Properties
         public string TestText { get; set; } = "Das ist ein Test";
+        public Visibility CallButtonIsVisible
+        {
+            get { return _callButtonIsVisible; }
+            set { _callButtonIsVisible = value; }
+        }
         #endregion
     }
 }
